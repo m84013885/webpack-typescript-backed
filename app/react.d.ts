d7af97b14c1537b38c351c03ed9e0ce4cf812d64
m84013885 <84013885@qq.com>
2021-04-27 16:47:41 +0800

    fix

--- app/react.d.ts
+++ app/react.d.ts
@@ -1,9 +1,14 @@
-declare const React: any
-declare const ReactDOM: any
-declare const useState: any
-declare const useEffect: any
-declare const useCallback: any
-declare const useMemo: any
-declare const useReducer: any
-declare const useRef: any
-declare const useContext: any
+import * as _React from "react"
+import * as _ReactDOM from "react-dom"
+
+declare global {
+    const React: typeof _React;
+    const ReactDOM: typeof _ReactDOM;
+    const useState: typeof _React.useState;
+    const useEffect: typeof _React.useEffect;
+    const useCallback: typeof _React.useCallback;
+    const useMemo: typeof _React.useMemo;
+    const useReducer: typeof _React.useReducer;
+    const useRef: typeof _React.useRef;
+    const useContext: typeof _React.useContext;
+}
