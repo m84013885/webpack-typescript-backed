b9f1a9b932388eceee9d4cffe06d5e04a7cc7bc0
m84013885 <84013885@qq.com>
2021-05-11 11:34:56 +0800

    feat : 实验性

--- app/page/content/space/index.tsx
+++ app/page/content/space/index.tsx
@@ -1,47 +1,13 @@
-import style from "./index.css"
+// import style from "./index.css"
+import ReactFlow from 'react-flow-renderer';
 
-function Space() {
+const elements = [
+    { id: '1', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
+    // you can also pass a React component as a label
+    { id: '2', data: { label: <div>Node 2</div> }, position: { x: 100, y: 100 } },
+    { id: 'e1-2', source: '1', target: '2', animated: true },
+];
 
-    return (
-        <>
-            <svg xmlns="http://www.w3.org/2000/svg">
-                
-            </svg>
-            <div className={style.page}>
-                <div className={style.row}>
-                    <div className={style.btn}></div>
-                </div>
-                <div className={style.row}>
-                    <div className={style.btn}></div>
-                </div>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" width="200px" height="40px">
-                <rect width="200px" height="40px" fill="transparent" onClick={() => { console.log(5) }}>
-                </rect>
-                <path
-                    stroke-width="3"
-                    stroke-linecap="round"
-                    d="M4.566,-7.433C14.787,4.415,38.353,18.5,69.6,18.5"
-                    stroke="black"
-                    fill="transparent"
-                />
-                <g transform="translate(100,0)" id="shape1"><path stroke="#454545" stroke-width="3" stroke-linecap="round" d="M4.566,-7.433C14.787,4.415,38.353,18.5,69.6,18.5" fill="none"></path></g>
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="200px" height="40px">
-                    <rect width="200px" height="40px" fill="transparent" onClick={() => { console.log(5) }}>
-                    </rect>
-                    <path
-                        d="
-                                M 0 0
-                                L 200 40
-                                Z
-                            "
-                        stroke="black"
-                        fill="transparent"
-                    />
-                </svg> */}
-            </div>
-        </>
-    );
-}
+const BasicFlow = () => <ReactFlow elements={elements} />;
 
-export default Space
+export default BasicFlow
